ecd489d1d8ea8a42032f9ac06686f1cd2a39a9ad
Irfan Azis Al Rasyid <92372601+Iruyizu1@users.noreply.github.com>
2023-11-21 10:26:25 +0700

    nice

--- Hello.py
+++ Hello.py
@@ -14,6 +14,13 @@
 
 import streamlit as st
 from streamlit.logger import get_logger
+import pandas as pd
+import numpy as np
+import random
+from deap import base
+from deap import creator
+from deap import tools
+import matplotlib.pyplot as plt
 
 LOGGER = get_logger(__name__)
 
@@ -29,21 +36,7 @@ def run():
     st.sidebar.success("Select a demo above.")
 
     st.markdown(
-        """
-        Streamlit is an open-source app framework built specifically for
-        Machine Learning and Data Science projects.
-        **👈 Select a demo from the sidebar** to see some examples
-        of what Streamlit can do!
-        ### Want to learn more?
-        - Check out [streamlit.io](https://streamlit.io)
-        - Jump into our [documentation](https://docs.streamlit.io)
-        - Ask a question in our [community
-          forums](https://discuss.streamlit.io)
-        ### See more complex demos
-        - Use a neural net to [analyze the Udacity Self-driving Car Image
-          Dataset](https://github.com/streamlit/demo-self-driving)
-        - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
-    """
+
     )
 
 
